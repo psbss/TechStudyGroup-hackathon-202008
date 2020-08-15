428aa6e823904154ab5f271cb927c04121e1271b
yuu <uypsbss@gmail.com>
2020-08-15 20:02:30 +0900

    package lock

--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,79 @@
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const webpack = require("webpack");
+// MODE ? production or development
+const MODE = "production";
+const enableSourceMap = MODE === "development";
+
 module.exports = {
-  mode: "development",
+  // エントリーファイル
+  entry: `./src/js/index.js`,
+  // ファイルの出力先
+  output: {
+    path: `${__dirname}/dist`,
+    filename: "index_bundle.js",
+  },
+  mode: MODE,
+  // デベロッパーサーバの設定
+  devServer: {
+    contentBase: "dist",
+    open: true,
+  },
   module: {
     rules: [
       {
-        test: /\.css$/,
+        // sass or scss ファイルを対象とする
+        test: /\.(sa|sc|c)ss$/,
+        // node_modules は対象外
+        exclude: /node_modules/,
         use: [
-          "style-loader",
-          "css-loader"
-        ]
-      }
-    ]
-  }
+          {
+            loader: MiniCssExtractPlugin.loader,
+          },
+          {
+            loader: "css-loader",
+            options: {
+              // .css 内でのinport
+              url: false,
+              // sourceMap の出力
+              sourceMap: enableSourceMap,
+
+              // 0 => no loaders (default);
+              // 1 => postcss-loader;
+              // 2 => postcss-loader, sass-loader
+              importLoaders: 2,
+            },
+          },
+          {
+            // sass のロード
+            loader: "sass-loader",
+            options: {
+              // sourceMap の 出力
+              sourceMap: enableSourceMap,
+            },
+          },
+        ],
+      },
+      {
+        // html ファイルを対象とする
+        test: /\.html$/,
+        loader: "html-loader",
+      },
+    ],
+  },
+  plugins: [
+    // CSSファイルを外に置く処理
+    new MiniCssExtractPlugin({
+      // ファイル名の指定
+      filename: "style.css",
+    }),
+    // html をsrcからdistに移す処理
+    new HtmlWebpackPlugin({
+      template: "src/html/index.html",
+    }),
+    // jquery settings
+    new webpack.ProvidePlugin({
+      $: "jquery",
+    }),
+  ],
 };
