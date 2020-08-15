2dc1f704aa8082335842ca1e357465639b16ba34
Miguel190751 <69588669+Miguel190751@users.noreply.github.com>
2020-08-15 23:02:02 +0900

    Update Input-Json.sh
    以下2点改善しました！
    
    ・作品2 .. 5のタイトルで n/a が入力された場合にWorks の入力を終了する
    ・Works は全ての項目に　「存在しない場合は N/A と入力」　と表示

--- Input-Json.sh
+++ Input-Json.sh
@@ -41,6 +41,27 @@ keyMain2='"main2":'
 keyMain3='"main3":'
 keyFooter='"footer":'
 
+title2Json="${keyTitle2}"
+term2Json="${keyTerm2}"
+ss2Json="${keySS2}"
+comment2Json="${keyComment2}"
+
+title3Json="${keyTitle3}"
+term3Json="${keyTerm3}"
+ss3Json="${keySS3}"
+comment3Json="${keyComment3}"
+
+title4Json="${keyTitle4}"
+term4Json="${keyTerm4}"
+ss4Json="${keySS4}"
+comment4Json="${keyComment4}"
+
+title5Json="${keyTitle5}"
+term5Json="${keyTerm5}"
+ss5Json="${keySS5}"
+comment5Json="${keyComment5}"
+
+
 
 read -p "名前を入力してください！: " NAME
 nameJson="${keyName} \"$NAME\""
@@ -87,56 +108,65 @@ ss1Json="${keySS1} \"$SS1\""
 read -p "作品1のコメントを入力してください！: " COMMENT1
 comment1Json="${keyComment1} \"$COMMENT1\""
 
-read -p "作品2のタイトルを入力してください！: " TITLE2
-title2Json="${keyTitle2} \"$TITLE2\""
+read -p "作品2のタイトルを入力してください！(無い場合は「n/a」と入力してください): " TITLE2
 
-read -p "作品2の製作期間を入力してください！: " TERM2
-term2Json="${keyTerm2} \"$TERM2\""
+if [ $TITLE2 != "n/a" ]; then
+ title2Json="${keyTitle2} \"$TITLE2\""
 
-read -p "作品2のSSを入力してください！: " SS2
-ss2Json="${keySS2} \"$SS2\""
+ read -p "作品2の製作期間を入力してください！: " TERM2
+ term2Json="${keyTerm2} \"$TERM2\""
 
-read -p "作品2のコメントを入力してください！: " COMMENT2
-comment2Json="${keyComment2} \"$COMMENT2\""
+ read -p "作品2のSSを入力してください！: " SS2
+ ss2Json="${keySS2} \"$SS2\""
 
-read -p "作品3のタイトルを入力してください！: " TITLE3
-title3Json="${keyTitle3} \"$TITLE3\""
+ read -p "作品2のコメントを入力してください！: " COMMENT2
+ comment2Json="${keyComment2} \"$COMMENT2\""
 
-read -p "作品3の製作期間を入力してください！: " TERM3
-term3Json="${keyTerm3} \"$TERM3\""
+ read -p "作品3のタイトルを入力してください！(無い場合は「n/a」と入力してください): " TITLE3 
+ if [ $TITLE3 != "n/a" ]; then
+  title3Json="${keyTitle3} \"$TITLE3\""
 
-read -p "作品3のSSを入力してください！: " SS3
-ss3Json="${keySS3} \"$SS3\""
+  read -p "作品3の製作期間を入力してください！: " TERM3
+  term3Json="${keyTerm3} \"$TERM3\""
 
-read -p "作品3のコメントを入力してください！: " COMMENT3
-comment3Json="${keyComment3} \"$COMMENT3\""
+  read -p "作品3のSSを入力してください！: " SS3
+  ss3Json="${keySS3} \"$SS3\""
 
-read -p "作品4のタイトルを入力してください！: " TITLE4
-title4Json="${keyTitle4} \"$TITLE4\""
+  read -p "作品3のコメントを入力してください！: " COMMENT3
+  comment3Json="${keyComment3} \"$COMMENT3\""
 
-read -p "作品4の製作期間を入力してください！: " TERM4
-term4Json="${keyTerm4} \"$TERM4\""
+  read -p "作品4のタイトルを入力してください！(無い場合は「n/a」と入力してください): " TITLE4
+  if [ $TITLE4 != "n/a" ]; then 
+   title4Json="${keyTitle4} \"$TITLE4\""
 
-read -p "作品4のSSを入力してください！: " SS4
-ss4Json="${keySS4} \"$SS4\""
+   read -p "作品4の製作期間を入力してください！: " TERM4
+   term4Json="${keyTerm4} \"$TERM4\""
 
-read -p "作品4のコメントを入力してください！: " COMMENT4
-comment4Json="${keyComment4} \"$COMMENT4\""
+   read -p "作品4のSSを入力してください！: " SS4
+   ss4Json="${keySS4} \"$SS4\""
 
-read -p "作品5のタイトルを入力してください！: " TITLE5
-title5Json="${keyTitle5} \"$TITLE5\""
+   read -p "作品4のコメントを入力してください！: " COMMENT4
+   comment4Json="${keyComment4} \"$COMMENT4\""
 
-read -p "作品5の製作期間を入力してください！: " TERM5
-term5Json="${keyTerm5} \"$TERM5\""
+   read -p "作品5のタイトルを入力してください！(無い場合は「n/a」と入力してください): " TITLE5
+   if [ $TITLE5 != "n/a" ]; then 
+    title5Json="${keyTitle5} \"$TITLE5\""
 
-read -p "作品5のSSを入力してください！: " SS5
-ss5Json="${keySS5} \"$SS5\""
+    read -p "作品5の製作期間を入力してください！: " TERM5
+    term5Json="${keyTerm5} \"$TERM5\""
 
-read -p "作品5のコメントを入力してください！: " COMMENT5
-comment5Json="${keyComment5} \"$COMMENT5\""
+    read -p "作品5のSSを入力してください！: " SS5
+    ss5Json="${keySS5} \"$SS5\""
 
-read -p "メッセージのheaderを入力してください！: " COMMENT
-headerJson="${keyHeader} \"$COMMENT\""
+    read -p "作品5のコメントを入力してください！: " COMMENT5
+    comment5Json="${keyComment5} \"$COMMENT5\""
+   fi
+  fi
+ fi
+fi
+
+    read -p "メッセージのheaderを入力してください！: " COMMENT
+    headerJson="${keyHeader} \"$COMMENT\""
 
 read -p "本文1を入力してください！: " MAIN1
 main1Json="${keyMain1} \"$MAIN1\""
