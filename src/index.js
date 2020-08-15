3af4a1909b9e512c51dd78a73bc624b4353a0fe2
yuu <uypsbss@gmail.com>
2020-08-15 14:32:58 +0900

    Fetch API を利用して json data を取得する

--- src/index.js
+++ src/index.js
@@ -1,9 +1,29 @@
 const url = "./data.json";
+let tags;
 
-async function main() {
-  const res = await fetch(url);
-  const data = await res.json();
-  console.log(data);
-};
+function dataCheck(myjson) {
+  const keys = Object.keys(myjson);
+  console.log(keys.length);
+  for (let i = 0; i < keys.length; i++) {
+    let res = document.getElementById(`${keys[i]}`);
+  }
+}
+
+function getData(url) {
+  fetch(url, {
+  }).then((response) => {
+    return response.json();
+  }).then((myjson) => {
+    console.log(myjson);
+    dataCheck(myjson);
+  }).catch((e) => {
+    console.log("e");
+    console.error(e);
+  })
+}
+
+const main = () => {
+  getData(url);
+}
 
 main();
