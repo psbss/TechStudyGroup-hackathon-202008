afd0f5ffc604d08633f814fe6d4ada2ee7db2e73
yuu <uypsbss@gmail.com>
2020-08-15 20:04:59 +0900

    add / change file

---
+++ src/js/index.js
@@ -0,0 +1,5 @@
+import "../css/main.scss";
+import "bootstrap";
+import "@fortawesome/fontawesome-free/js/fontawesome";
+import "@fortawesome/fontawesome-free/js/solid";
+import "@fortawesome/fontawesome-free/js/regular";
